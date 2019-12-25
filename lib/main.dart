72a1880a20b3c41b29356b54fdd9ddcc071ecc7b
Arcaneless <kings88819@gmail.com>
2019-12-26 01:11:41 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator/calculator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Calculator()
+    );
+  }
+}
